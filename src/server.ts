750c347071d987227aa00a700f6b95bc28b1c212
solebellsBEACH <lucassxxavier@gmail.com>
2022-08-11 19:44:55 -0300

    trocando porta

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,4 @@ route.get('/', (req: Request, res: Response) => {
 
 app.use(route)
 
-app.listen(3333, () => 'server running on port 3333')
+app.listen(process.env.PORT || 3000, () => 'server running on port 3333')
