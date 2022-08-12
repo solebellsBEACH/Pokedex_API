d09add894ea6bcad8956e0ce9a813e41db98b8aa
solebellsBEACH <lucassxxavier@gmail.com>
2022-08-12 16:17:02 -0300

    corrigindo bugs

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-const mongoConector = require('./config/mongoose-connector');
-const userRoutes = require('./config/routes/user/routes')
-const userPrivateRoutes = require('./config/routes/user/private.routes')
+const mongoConector = require('./core/config/mongoose-connector');
+const userRoutes = require('./core/entities/user/routes/routes')
+const userPrivateRoutes = require('./core/entities/user/routes/private.routes')
 
 const app = express();
 
@@ -10,10 +10,10 @@ require('dotenv').config()
 const { MONGO_URI } = process.env
 
 mongoConector(MONGO_URI)
+
 //Routes 
 app.use('/api/user', userRoutes)
 app.use('/api/user', userPrivateRoutes)
-
 //
 
 app.listen(process.env.PORT || 3000, () => 'server running on port 3333')
