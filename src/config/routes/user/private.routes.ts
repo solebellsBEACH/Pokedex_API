e4917df4015726095fed0cb1e3564a00ad1be2ca
solebellsBEACH <lucassxxavier@gmail.com>
2022-08-12 14:59:13 -0300

    corrigindo erros de build

--- src/config/routes/user/private.routes.ts
+++ src/config/routes/user/private.routes.ts
@@ -1,9 +1,9 @@
 const authMiddleware = require('../../middlewares/auth')
-const express = require('express');
-const router = express.Router();
-const UserControllers = require('../../../controllers/user.controller')
+const expressPrivate = require('express');
+const routerPrivate = expressPrivate.Router();
+const UserControllersPrivate = require('../../../controllers/user.controller')
 
-router.use(authMiddleware)
-router.get('/', UserControllers.getUser)
+routerPrivate.use(authMiddleware)
+routerPrivate.get('/', UserControllersPrivate.getUser)
 
-module.exports = router;
+module.exports = routerPrivate;
